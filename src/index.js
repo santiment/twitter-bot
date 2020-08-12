47a86fcaeda92335cbb74c865e952f76f5fb1052
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-08-12 14:27:55 +0300

    Update README with env vars usage example

--- src/index.js
+++ src/index.js
@@ -5,7 +5,3 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Hello Santiment!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-setInterval(function () {
-  console.log("Logging something every 10s");
-}, 10000);
