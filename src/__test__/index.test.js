9d4b4a4e463609e9b388a4219436148f84e4b320
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-08-12 10:29:25 +0300

    Add failing test

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -2,6 +2,6 @@ it("1 + 1 equals 2", async () => {
   expect(1 + 1).toEqual(2);
 });
 
-it("1 + 2 equals 3", async () => {
-  expect(1 + 2).toEqual(3);
+it("failing 1 + 1 equals 3", async () => {
+  expect(1 + 1).toEqual(3);
 });
